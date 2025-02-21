5e7e8f09eb688b1d623bea70d65d9ae2548f6301
Mysty47 <106233800+Mysty47@users.noreply.github.com>
2025-02-21 03:39:27 +0200

    homepage

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="app/")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     message = "Backend test"
-    return templates.TemplateResponse("home.html", {"request": request, "message": message})
+    return templates.TemplateResponse("login.html", {"request": request, "message": message})
+
+@app.get("/login.html", response_class=HTMLResponse)
+async def read_root(request: Request):
+    message = "Backend test"
+    return templates.TemplateResponse("login.html", {"request": request, "message": message})
